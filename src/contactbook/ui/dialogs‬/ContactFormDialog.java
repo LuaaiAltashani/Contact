cbbe93a77a821d3853236adf6a550fae27efd785
LuaaiAltashani <loaiy2k@yahoo.com>
2026-06-26 11:22:21 +0200

    Update ContactFormDialog.java

--- src/contactbook/ui/dialogs‬/ContactFormDialog.java
+++ src/contactbook/ui/dialogs‬/ContactFormDialog.java
@@ -5,6 +5,7 @@ import contactbook.model.Contact;
 import javax.swing.*;
 import java.awt.*;
 
+//نافذة اضافة و تعديل جهة اتصال
 public class ContactFormDialog extends BaseFormDialog {
     private final JTextField nameField = new JTextField(25);
     private final JTextField phoneField = new JTextField(25);
@@ -17,6 +18,7 @@ public class ContactFormDialog extends BaseFormDialog {
         notesArea.setLineWrap(true);
         notesArea.setWrapStyleWord(true);
 
+        //تعبئة الخانات للتعديل 
         if (initial != null) {
             nameField.setText(initial.getName());
             phoneField.setText(initial.getPhone());
@@ -24,6 +26,7 @@ public class ContactFormDialog extends BaseFormDialog {
             notesArea.setText(initial.getNotes());
         }
 
+        //نشاء GridBagLayout مع 4 خانات
         JPanel form = new JPanel(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
         gc.insets = new Insets(6, 6, 6, 6);
@@ -55,7 +58,7 @@ public class ContactFormDialog extends BaseFormDialog {
         JButton cancelBtn = new JButton("إلغاء");
         okBtn.addActionListener(e -> {
             if (nameField.getText().trim().isEmpty() || phoneField.getText().trim().isEmpty()) {
-                JOptionPane.showMessageDialog(this, "الرجاء إدخال الاسم ورقم الهاتف.", "تنبيه", JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(this, "تأكد من إدخال الاسم ورقم الهاتف", "تنبيه", JOptionPane.WARNING_MESSAGE);
                 return;
             }
             closeWithSuccess();
@@ -69,19 +72,21 @@ public class ContactFormDialog extends BaseFormDialog {
         setLocationRelativeTo(owner);
     }
 
+    //انشاء object للبيانات المدرجة
     public Contact getResult(Contact base) {
         if (!isConfirmed()) return null;
-        Contact c = base == null ? new Contact() : base;
+        Contact c = base == null ? new Contact() : base; // ينشئ جديد في حال base = null
         c.setName(nameField.getText().trim());
         c.setPhone(phoneField.getText().trim());
-        c.setEmail(emptyToNull(emailField.getText()));
+        c.setEmail(emptyToNull(emailField.getText())); //للخانات الاختيارية 
         c.setNotes(emptyToNull(notesArea.getText()));
         return c;
     }
 
+    //تعيد null في حال النص فارغ
     private String emptyToNull(String s) {
         if (s == null) return null;
-        s = s.trim();
+        s = s.trim(); // ازالة الفراغ
         return s.isEmpty() ? null : s;
     }
 }
